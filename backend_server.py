dba74c6e685a7cebf05caa0a25ac34fcc9258878
Tim Henning <tim@luminosuslight.de>
2023-08-01 14:19:10 +0200

    Move PubMedBERT model to separate flask server to make restarts quicker

--- backend_server.py
+++ backend_server.py
@@ -1,19 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import requests
 app = Flask(__name__)
 CORS(app) # This will enable CORS for all routes
-
-# ------
-import time
-
-import torch
-from transformers import AutoTokenizer, AutoModel
-
-import csv
-from pathlib import Path
-import time
-import uuid
-import json
 
 import weaviate
 import numpy as np
@@ -29,70 +18,11 @@ weaviate_client = weaviate.Client(
 )
 
 
-# from https://github.com/berenslab/pubmed-landscape/blob/main/pubmed_landscape_src/data.py
-
-@torch.no_grad()
-def generate_embeddings(abstracts, tokenizer, model, device):
-    """Generate embeddings using BERT-based model.
-    Code from Luca Schmidt.
-
-    Parameters
-    ----------
-    abstracts : list
-        Abstract texts.
-    tokenizer : transformers.models.bert.tokenization_bert_fast.BertTokenizerFast
-        Tokenizer.
-    model : transformers.models.bert.modeling_bert.BertModel
-        BERT-based model.
-    device : str, {"cuda", "cpu"}
-        "cuda" if torch.cuda.is_available() else "cpu".
-
-    Returns
-    -------
-    embedding_cls : ndarray
-        [CLS] tokens of the abstracts.
-    embedding_sep : ndarray
-        [SEP] tokens of the abstracts.
-    embedding_av : ndarray
-        Average of tokens of the abstracts.
-    """
-    # preprocess the input
-    print("create tokenizer...")
-    inputs = tokenizer(
-        abstracts,
-        padding=True,
-        truncation=True,
-        return_tensors="pt",
-        max_length=512,
-    ).to(device)
-
-    # inference
-    print("run model...")
-    begin = time.time()
-    outputs = model(**inputs)[0].cpu().detach()
-    end = time.time()
-    print(f"Time: {end - begin:.2f} sec")
-
-    embedding_av = torch.mean(outputs, [0, 1]).numpy()
-    embedding_sep = outputs[:, -1, :].numpy()
-    embedding_cls = outputs[:, 0, :].numpy()
-
-
-    return embedding_cls, embedding_sep, embedding_av
-
-
-# from https://github.com/berenslab/pubmed-landscape/blob/main/scripts/02-ls-data-obtain-BERT-embeddings.ipynb
-
-# specifying model
-checkpoint = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
-
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = AutoModel.from_pretrained(checkpoint)
-device = 'cpu'
-model = model.to(device)
-
-# ------
-
+def get_embedding(query: str) -> np.ndarray:
+    url = 'http://localhost:55180/api/embedding/pubmedbert'
+    data = {'query': query}
+    result = requests.post(url, json=data)
+    return np.asarray(result.json()["embedding"])
 
 
 @app.route('/')
@@ -107,7 +37,7 @@ def query():
     if not query:
         return jsonify([])
 
-    _, embedding, _ = generate_embeddings(query, tokenizer, model, device)
+    embedding = get_embedding(query)
 
     response = (
         weaviate_client.query
@@ -134,7 +64,7 @@ def map_html():
     if not query:
         return jsonify([])
 
-    _, embedding, _ = generate_embeddings(query, tokenizer, model, device)
+    embedding = get_embedding(query)
 
     response = (
         weaviate_client.query
