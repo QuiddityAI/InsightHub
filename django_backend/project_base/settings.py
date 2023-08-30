599414627d25ec7d0919cea5eb1214ab23be3a72
Tim Henning <tim@luminosuslight.de>
2023-08-30 10:17:27 +0200

    Improve admin view by settings icons using Jazzmin settings

--- django_backend/project_base/settings.py
+++ django_backend/project_base/settings.py
@@ -174,3 +174,27 @@ LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'home'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+
+JAZZMIN_SETTINGS = {
+    # Hide these models when generating side menu (e.g auth.user)
+    "hide_models": [],
+
+    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
+    "order_with_respect_to": ["auth", "auth.user", "auth.group", "data_map_backend", "data_map_backend.organization",
+                              "data_map_backend.objectschema", "data_map_backend.objectfield", "data_map_backend.generator",
+                              "data_map_backend.embeddingspace"],
+
+    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
+    # for the full list of 5.13.0 free icon classes
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.group": "fas fa-users",
+        "data_map_backend.organization": "fas fa-building",
+        "data_map_backend.objectschema": "fas fa-database",
+        "data_map_backend.objectfield": "fas fa-grip-lines",
+        "data_map_backend.generator": "fas fa-microchip",
+        "data_map_backend.embeddingspace": "fas fa-globe",
+    },
+}
