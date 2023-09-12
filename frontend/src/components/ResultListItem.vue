9bbea033a88ef7d3e7cf68ae0d5faf48215f5e4e
Tim Henning <tim@luminosuslight.de>
2023-09-12 10:56:50 +0200

    Fix rendering of search result meta information

--- frontend/src/components/ResultListItem.vue
+++ frontend/src/components/ResultListItem.vue
@@ -17,6 +17,9 @@ export default {
     <p class="mt-2 text-xs leading-5 text-gray-700"><div v-html="rendering.body(item)"></div></p>
     <p class="mt-2 text-xs leading-5 text-gray-700"><a :href="rendering.url(item)">Link</a></p>
     <img class="w-10" :src="rendering.url(item)">
-    <span class="text-xs text-gray-500">{{ item._reciprocal_rank_score.toFixed(2) }} {{ item._source_scores }} {{ item._source_types }} {{ item._source_fields }},  {{ item._source_queries }}</span>
+    <span class="mr-3 px-2 text-xs text-gray-500 rounded-xl bg-gray-200">{{ item._reciprocal_rank_score.toFixed(2) }}</span>
+    <span v-for="origin in item._origins" class="mr-3 px-2 text-xs text-gray-500 rounded-xl bg-gray-200">
+      {{ origin.score.toFixed(2) }}, {{ origin.type }}: {{ origin.field }}, q = {{ origin.query }}
+    </span>
   </div>
 </template>
