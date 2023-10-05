75d27ab4f6756fd48131cdc82b5ce1d9b9398116
Tim Henning <tim@luminosuslight.de>
2023-10-05 12:51:09 +0200

    Tidy up method for getting cluster titles

--- data_backend/logic/clusters_and_titles.py
+++ data_backend/logic/clusters_and_titles.py
@@ -23,24 +23,18 @@ def get_cluster_titles(cluster_labels, positions, results, descriptive_text_fiel
     if num_clusters <= 0:
         return []
     texts_per_cluster = [""] * num_clusters
-    points_per_cluster_x = [[] for i in range(num_clusters)]
-    points_per_cluster_y = [[] for i in range(num_clusters)]
-    results_by_cluster = [[] for i in range(num_clusters)]
+    point_positions_per_cluster = [[] for i in range(num_clusters)]
 
     for result_index, cluster_index in enumerate(cluster_labels):
         if cluster_index <= -1: continue
         text = " ".join([results[result_index].get(field, "") for field in descriptive_text_fields])
         texts_per_cluster[cluster_index] += text
-        points_per_cluster_x[cluster_index].append(positions[result_index][0])
-        points_per_cluster_y[cluster_index].append(positions[result_index][1])
-        results_by_cluster[cluster_index].append(results[result_index])
-    timings.log("combining abstracts from search results")
+        point_positions_per_cluster[cluster_index].append(positions[result_index])
+    timings.log("collect information for clusters")
 
     # highlight TF-IDF words:
-    # tf_idf_helper = ClusterTitles()
     # vectorizer = TfidfVectorizer(stop_words="english")
     vectorizer = TfidfVectorizer(analyzer=tokenize, max_df=0.7)
-    # vectorizer = TfidfVectorizer(analyzer=tf_idf_helper.tokenize)
     tf_idf_matrix = vectorizer.fit_transform(texts_per_cluster)  # not numpy but scipy sparse array
     words = vectorizer.get_feature_names_out()
 
@@ -48,11 +42,11 @@ def get_cluster_titles(cluster_labels, positions, results, descriptive_text_fiel
 
     for cluster_index in range(num_clusters):
         # converting scipy sparse array to numpy using toarray() and selecting the only row [0]
-        sort_indexes_of_important_words = np.argsort(tf_idf_matrix[cluster_index].toarray()[0])
+        sort_indexes_of_important_words = np.argsort(tf_idf_matrix[cluster_index].toarray()[0])  # type: ignore
         most_important_words = words[sort_indexes_of_important_words[-3:]][::-1]
-        cluster_center = (float(np.mean(points_per_cluster_x[cluster_index])), float(np.mean(points_per_cluster_y[cluster_index])))
-        cluster_title = ", ".join(list(most_important_words)) + f" ({len(results_by_cluster[cluster_index])})"
+        cluster_center = np.mean(point_positions_per_cluster[cluster_index], axis=0).tolist()
+        cluster_title = ", ".join(list(most_important_words)) + f" ({len(point_positions_per_cluster[cluster_index])})"
         cluster_data.append({"id": cluster_index, "title": cluster_title, "center": cluster_center})
-    timings.log("Tf-Idf")
+    timings.log("Tf-Idf for cluster titles")
 
     return cluster_data
