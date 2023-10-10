51078c9addc9b93b9b390669b0be35cc8efee9ce
Tim Henning <tim@luminosuslight.de>
2023-10-10 13:46:58 +0200

    Remove unused code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,15 +7,6 @@ import MainNavigation from './components/MainNavigation.vue';
 export default {
   data() {
     return {
-      available_databases: [
-        {id: "absclust", title: "AbsClust Database", subtitle: "60 Million Documents"},
-        {id: "pubmed", title: "PubMed Database", subtitle: "5 Mio. Documents (of 20M)"},
-      ],
-      database_information: {
-        "absclust": "60 Million Documents",
-        "pubmed": "5 Mio. Documents (of 20M)",
-      },
-      selected_database: "absclust",
     }
   }
 }
