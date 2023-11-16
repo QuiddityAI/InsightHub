e6a645e2189bb9d553e14284bd2e5afd1651107a
Tim Henning <tim@luminosuslight.de>
2023-11-16 11:02:49 +0100

    Add type hints to get_pipeline_steps() using Copilot

--- data_backend/logic/extract_pipeline.py
+++ data_backend/logic/extract_pipeline.py
@@ -1,35 +1,36 @@
 import json
 import logging
+from typing import Callable, Optional
 
 from utils.dotdict import DotDict
 from logic.generator_functions import get_generator_function_from_field
 
 
 # TODO: add changed_at as parameter and cache function (using changed_at as measure for dropping the cache)
-def get_pipeline_steps(dataset: dict, ignored_fields: list[str] = [], enabled_fields: list[str] = [], only_fields: list[str] = []) -> tuple[list[list[dict]], set[str], set[str]]:
-    dataset = DotDict(dataset)
+def get_pipeline_steps(dataset_: dict, ignored_fields: list[str] = [], enabled_fields: list[str] = [], only_fields: list[str] = []) -> tuple[list[list[dict]], set[str], set[str]]:
+    dataset: DotDict = DotDict(dataset_)
     if has_circular_dependency(dataset):
         logging.error(f"The pipeline steps have a circular dependency, object dataset: {dataset.name}")
         logging.error(f"No pipeline steps will be executed at all for this dataset until this is fixed.")
         return [], set(), set()
 
-    required_fields = set()
-    potentially_changed_fields = set()
-    pipeline_steps = []
-    steps_added = []
-    any_field_skipped = True
+    required_fields: set[str] = set()
+    potentially_changed_fields: set[str] = set()
+    pipeline_steps: list[list[dict]] = []
+    steps_added: list[str] = []
+    any_field_skipped: bool = True
     while any_field_skipped:
-        phase_steps = []
-        steps_added_this_phase = []
+        phase_steps: list[dict] = []
+        steps_added_this_phase: list[str] = []
         any_field_skipped = False
         for field in dataset.object_fields.values():
             if field.identifier in steps_added: continue
-            this_field_skipped = False
+            this_field_skipped: bool = False
 
             if ((not only_fields and field.should_be_generated and not field.identifier in ignored_fields)
                     or field.identifier in only_fields
                     or field.identifier in enabled_fields):
-                dependencies = field.source_fields
+                dependencies: list[str] = field.source_fields
                 for dep in dependencies:
                     if dataset.object_fields[dep].generator and dep not in steps_added:
                         this_field_skipped = True
@@ -39,8 +40,8 @@ def get_pipeline_steps(dataset: dict, ignored_fields: list[str] = [], enabled_fi
                     any_field_skipped = True
                     continue
 
-                generator_function = get_generator_function_from_field(field)
-                condition_function = eval(field.generating_condition) if field.generating_condition else None
+                generator_function: Callable = get_generator_function_from_field(field)
+                condition_function: Optional[Callable] = eval(field.generating_condition) if field.generating_condition else None
 
                 required_fields |= set(field.source_fields)
                 potentially_changed_fields.add(field.identifier)
