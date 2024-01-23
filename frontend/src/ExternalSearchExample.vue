899d80642ac12f42176a6eb0552e27597ab18cd7
Tim Henning <tim@luminosuslight.de>
2024-01-23 18:04:58 +0100

    Add some hypothetical example URLs to embed the map

--- frontend/src/ExternalSearchExample.vue
+++ frontend/src/ExternalSearchExample.vue
@@ -45,7 +45,7 @@ export default {
         <div class="flex-1 h-9 flex flex-row items-center mb-4">
           <span class="mr-4">Search:</span>
           <input type="search" name="search" @search=""
-          :placeholder="`Describe what  you want to find`"
+          :placeholder="`Describe what you want to find`"
           class="w-full h-full rounded-md border-0 py-1.5 text-gray-900 ring-1
         ring-inset ring-gray-300 placeholder:text-gray-400
         focus:ring-2 focus:ring-inset focus:ring-blue-400
@@ -55,6 +55,15 @@ export default {
         <iframe
           allowfullscreen="true"
           src="embed.html?dataset_id=6&map_id=72fb88b3e8159ad275364753fe7a4b00"
+
+          _option_without_access_to_their_db_src="absclust.com/embed.html?item_set=www.xyz_tool.com/result_set_1234.json"
+
+          _option_with_access_to_their_db_src="absclust.com/embed.html?client=xyz_tool&item_ids=1234,5678,9012"
+
+          _option_with_access_to_their_db_and_query_src="absclust.com/embed.html?client=xyz_tool&query=mxene"
+
+          _option_with_our_db_and_query_src="absclust.com/embed.html?query=mxene"
+
           width="100%"
           style="max-width: 1200px; min-height: 500px"
         >
