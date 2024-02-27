c3f74bd2be89126645f89d12ecf245c9e19dba7e
Tim Henning <tim@luminosuslight.de>
2024-02-27 12:35:26 +0100

    Add empty component template for convenience

---
+++ frontend/src/components/EmptyTemplate.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { useToast } from 'primevue/usetoast';
+import { httpClient, djangoClient } from "../api/httpClient"
+import { mapStores } from "pinia"
+import { useAppStateStore } from "../../stores/app_state_store"
+import { useMapStateStore } from "../../stores/map_state_store"
+const appState = useAppStateStore()
+const mapState = useMapStateStore()
+const toast = useToast()
+</script>
+
+<script>
+
+export default {
+  inject: ["eventBus"],
+  props: [],
+  emits: [],
+  data() {
+    return {
+    }
+  },
+  computed: {
+    ...mapStores(useMapStateStore),
+    ...mapStores(useAppStateStore),
+  },
+  mounted() {
+  },
+  watch: {
+  },
+  methods: {
+  },
+}
+</script>
+
+<template>
+  <div>
+
+  </div>
+
+</template>
+
+<style scoped>
+</style>
