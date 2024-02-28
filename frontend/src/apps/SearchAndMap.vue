cf09a9cbc31474aba2b43adabe81c765e07de75c
Tim Henning <tim@luminosuslight.de>
2024-02-28 12:32:37 +0100

    Fix variable name in Add Current Map button

--- frontend/src/apps/SearchAndMap.vue
+++ frontend/src/apps/SearchAndMap.vue
@@ -413,7 +413,7 @@ export default {
                 Log in to store maps
               </Message>
 
-              <div v-if="appState.map_id && appState.logged_in" class="my-2 flex items-stretch">
+              <div v-if="appState.map_task_id && appState.logged_in" class="my-2 flex items-stretch">
                 <button
                   class="flex-auto rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6"
                   type="button"
