7b10caebbff18acda45c11aa8d4198cd35dd832e
Tim Henning <tim@luminosuslight.de>
2024-03-07 19:44:44 +0100

    Make defaults field in Dataset optional

--- organization_backend/data_map_backend/models.py
+++ organization_backend/data_map_backend/models.py
@@ -364,8 +364,8 @@ class Dataset(models.Model):
         verbose_name="Other Defaults",
         help_text="Default values for map parameters",
         default=dict,
-        blank=False,
-        null=False)
+        blank=True,
+        null=True)
     result_list_rendering = models.JSONField(
         verbose_name="Result List Rendering",
         default=get_default_result_list_rendering,
