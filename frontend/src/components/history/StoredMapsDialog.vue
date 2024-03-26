9627f93031c058dd97673ea6744a2836c84b7dec
Tim Henning <tim@luminosuslight.de>
2024-03-26 17:07:32 +0100

    Fix storing and loading maps in UI

--- frontend/src/components/history/StoredMapsDialog.vue
+++ frontend/src/components/history/StoredMapsDialog.vue
@@ -14,7 +14,7 @@ const toast = useToast()
 <script>
 
 export default {
-  inject: ["eventBus"],
+  inject: ["eventBus", "dialogRef"],
   props: [],
   emits: [],
   data() {
@@ -40,7 +40,7 @@ export default {
       Log in to store maps
     </Message>
 
-    <div v-if="appState.map_task_id && appState.logged_in" class="my-2 flex items-stretch">
+    <div v-if="appState.map_id && appState.logged_in" class="my-2 flex items-stretch">
       <button
         class="flex-auto rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6"
         type="button"
@@ -63,7 +63,7 @@ export default {
             Delete
           </button>
           <button
-            @click="appState.show_stored_map(stored_map.id)"
+            @click="appState.show_stored_map(stored_map.id); dialogRef.close()"
             class="text-sm font-light text-gray-500 hover:text-blue-500/50">
             Show Map
           </button>
