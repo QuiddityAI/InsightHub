ba6794dc4fcac4645d50019ecac45eb6009532dd
Tim Henning <tim@luminosuslight.de>
2024-04-01 19:26:36 +0200

    Adapt chunk generator to return dicts with metadata

--- data_backend/logic/chunking.py
+++ data_backend/logic/chunking.py
@@ -20,5 +20,15 @@ def chunk_text_generator(source_fields_list_batch: Iterable[Iterable[str | Itera
 def chunk_text(text: str, chunk_size_in_characters: int, overlap_in_characters: int):
     chunks = []
     for i in range(0, len(text), chunk_size_in_characters - overlap_in_characters):
-        chunks.append(text[i:i + chunk_size_in_characters])
+        text = text[i:i + chunk_size_in_characters]
+        chunks.append({
+            'prefix': '',
+            'text': text,
+            'suffix': '',
+            'start': i,
+            'end': i + len(text) - 1,
+            'page': None,
+            'coordinates': None,
+            'section': None,
+            })
     return chunks
