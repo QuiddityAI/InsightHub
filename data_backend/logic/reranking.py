dcc5e82aec614ca4903f590d61cae2e1df31a4b7
Tim Henning <tim@luminosuslight.de>
2024-04-13 12:25:02 +0200

    Fix caching of rerankings again, making sure the origins are set in any case

--- data_backend/logic/reranking.py
+++ data_backend/logic/reranking.py
@@ -16,16 +16,17 @@ co = cohere.Client(os.environ.get('COHERE_API_KEY', "no_api_key"))
 cache = Cache("/data/quiddity_data/reranking_cache/")
 
 
-
 def rerank(query: str, sorted_ids: list[tuple[int, str]], items_by_dataset: dict, top_n=10) -> list[tuple[int, str]]:
     cache_key = f"rerank_{query}_{json.dumps(sorted_ids[:top_n])}_{top_n}"
     if cache_key in cache:
         # logging.warning(f"Using reranking cache for query: {query}")
-        result = cache.get(cache_key)
-        assert isinstance(result, list)
-        return result
-    contexts = get_context_for_each_item_in_search_results(sorted_ids[:top_n], items_by_dataset)
-    response = _get_reranking_results(query, tuple(contexts), top_n)
+        response = cache.get(cache_key)
+        assert isinstance(response, cohere.RerankResponse)
+    else:
+        contexts = get_context_for_each_item_in_search_results(sorted_ids[:top_n], items_by_dataset)
+        response = _get_reranking_results(query, tuple(contexts), top_n)
+        cache.set(cache_key, response, expire=3600*24*7*4)  # 4 weeks
+
     new_indicies = [rerank_result.index for rerank_result in response.results]
     for rank, rerank_result in enumerate(response.results):
         idx = rerank_result.index
@@ -36,7 +37,6 @@ def rerank(query: str, sorted_ids: list[tuple[int, str]], items_by_dataset: dict
                           'query': query, 'score': rerank_result.relevance_score, 'rank': rank + 1})
 
     sorted_ids[:top_n] = [sorted_ids[i] for i in new_indicies]
-    cache.set(cache_key, sorted_ids, expire=3600*24*7*4)  # 4 weeks
     return sorted_ids
 
 
