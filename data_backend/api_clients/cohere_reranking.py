72d401336ebf285eec217d70718a1af7b2098bdb
Tim Henning <tim@luminosuslight.de>
2024-04-16 11:59:32 +0200

    Skip reranking if Cohere API key isn't there or expired (not sure if it works)

--- data_backend/api_clients/cohere_reranking.py
+++ data_backend/api_clients/cohere_reranking.py
@@ -12,11 +12,15 @@ co = cohere.Client(os.environ.get('COHERE_API_KEY', "no_api_key"))
 
 def get_reranking_results(query: str, texts: tuple[str, ...], top_n=10):
     # logging.warning(f"Using Cohere API to rerank {top_n} results for query: {query}")
-    response = co.rerank(
-        model = 'rerank-english-v3.0',  # multi-lingual is also available
-        query = query,
-        documents = texts,
-        top_n = top_n,
-        return_documents=False
-    )
+    try:
+        response = co.rerank(
+            model = 'rerank-english-v3.0',  # multi-lingual is also available
+            query = query,
+            documents = texts,
+            top_n = top_n,
+            return_documents=False
+        )
+    except Exception as e:
+        logging.error(f"Failed to rerank query: {query} with error: {e}")
+        response = cohere.RerankResponse(results=[cohere.RerankResponseResultsItem(index=i, relevance_score=0.0) for i in range(top_n)])
     return response
