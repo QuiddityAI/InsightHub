3e05d318ba33147e840b07a670e7dac879d235d1
Tim Henning <tim@luminosuslight.de>
2024-06-24 10:54:12 +0200

    Fix typo in download S2 dataset script

--- scripts_and_examples/download_semantic_scholar_dataset.py
+++ scripts_and_examples/download_semantic_scholar_dataset.py
@@ -1,8 +1,26 @@
 import requests
 import json
 
-# r1 = requests.get('https://api.semanticscholar.org/datasets/v1/release').json()
-# print(r1[-3:])
+from pathlib import Path
+import os
+
+def load_env_file():
+    with open("../.env", "r") as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            if "=" not in line:
+                continue
+            key, value = line.strip().split("=")
+            os.environ[key] = value
+
+load_env_file()
+
+API_KEY = os.getenv('SEMANTIC_SCHOLAR_API_KEY')
+
+
+releases = requests.get('https://api.semanticscholar.org/datasets/v1/release').json()
+print(releases[-3:])
 #['2023-08-29', '2023-09-05', '2023-09-12']
 
 # r2 = requests.get('https://api.semanticscholar.org/datasets/v1/release/latest').json()
@@ -16,8 +34,8 @@ import json
 # #     "README": "Semantic Scholar Academic Graph Datasets The "abstracts" dataset provides..."
 # # }
 
-r3 = requests.get('https://api.semanticscholar.org/datasets/v1/release/latest/dataset/papers').json()
-print(json.dumps(r3, indent=2))
+#r3 = requests.get('https://api.semanticscholar.org/datasets/v1/release/latest/dataset/papers').json()
+#print(json.dumps(r3, indent=2))
 # # {
 # #   "name": "abstracts",
 # #   "description": "Paper abstract text, where available. 100M records in 30 1.8GB files.",
