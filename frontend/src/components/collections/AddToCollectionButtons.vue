77b692620d7945e27aff89df0a32a9ef118f6ca4
Tim Henning <tim@luminosuslight.de>
2024-07-02 19:30:51 +0200

    Fix usage of app state store in AddToCollectionButtons

--- frontend/src/components/collections/AddToCollectionButtons.vue
+++ frontend/src/components/collections/AddToCollectionButtons.vue
@@ -69,7 +69,7 @@ export default {
 <template>
   <div class="flex flex-row h-7">
       <button class="flex flex-row items-center gap-2 border border-gray-300 rounded-l-md px-3 text-sm text-gray-500 hover:text-blue-500 hover:bg-gray-100"
-        @click="$emit('addToCollection', this.appStateStore.last_used_collection_id, this.appStateStore.last_used_collection_class, true)"
+        @click="$emit('addToCollection', appState.last_used_collection_id, appState.last_used_collection_class, true)"
         v-tooltip.bottom="{ value: 'Save this item in a collection', showDelay: 500 }">
         <BookmarkIcon class="h-4 w-4 text-blue-500"></BookmarkIcon> {{ selected_collection_name }}
       </button>
@@ -82,7 +82,7 @@ export default {
     <OverlayPanel ref="select_collection_overlay">
       <div class="flex flex-row gap-3">
         <select
-          v-model="this.appStateStore.last_used_collection_id"
+          v-model="appState.last_used_collection_id"
           @change="select_default_class"
           class="h-8 w-54 rounded-md border-transparent py-0 pl-2 pr-8 text-sm text-gray-500 ring-1 ring-gray-300 focus:border-blue-500 focus:ring-blue-500">
           <option v-for="collection in appState.collections" :value="collection.id">
@@ -90,11 +90,11 @@ export default {
           </option>
         </select>
         <select
-          v-if="this.appStateStore.last_used_collection_id !== null"
-          v-model="this.appStateStore.last_used_collection_class"
+          v-if="appState.last_used_collection_id !== null"
+          v-model="appState.last_used_collection_class"
           class="h-8 w-32 rounded-md border-transparent py-0 pl-2 pr-8 text-sm text-gray-500 ring-1 ring-gray-300 focus:border-blue-500 focus:ring-blue-500">
           <option
-            v-for="class_details in appState.collections.find((e) => e.id == this.appStateStore.last_used_collection_id).actual_classes"
+            v-for="class_details in appState.collections.find((e) => e.id == appState.last_used_collection_id).actual_classes"
             :value="class_details.name">
             {{ class_details.name == "_default" ? "Items" : class_details.name }}
           </option>
@@ -102,7 +102,7 @@ export default {
         <button
           title="Add to collection"
           @click="
-            $emit('addToCollection', this.appStateStore.last_used_collection_id, this.appStateStore.last_used_collection_class, true);
+            $emit('addToCollection', appState.last_used_collection_id, appState.last_used_collection_class, true);
             $refs.select_collection_overlay.hide()
           "
           class="mr-1 w-8 rounded-md px-2 text-green-600/50 ring-1 ring-gray-300 hover:bg-green-100">
@@ -111,7 +111,7 @@ export default {
         <button
           v-if="appState.dev_mode"
           @click="
-            $emit('addToCollection', this.appStateStore.last_used_collection_id, this.appStateStore.last_used_collection_class, false);
+            $emit('addToCollection', appState.last_used_collection_id, appState.last_used_collection_class, false);
             $refs.select_collection_overlay.hide()
           "
           class="w-8 rounded-md px-2 text-red-600/50 ring-1 ring-gray-300 hover:bg-red-100">
@@ -120,7 +120,7 @@ export default {
         <button
           v-if="multiple_items"
           @click="
-            $emit('removeFromCollection', this.appStateStore.last_used_collection_id, this.appStateStore.last_used_collection_class);
+            $emit('removeFromCollection', appState.last_used_collection_id, appState.last_used_collection_class);
             $refs.select_collection_overlay.hide()
           "
           class="w-8 rounded-md px-2 text-gray-300 ring-1 ring-gray-300 hover:bg-red-100"
