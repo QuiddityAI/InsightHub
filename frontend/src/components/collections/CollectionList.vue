ea6ac083a2c760d560fdedd3aa969d999d2362a3
Tim Henning <tim@luminosuslight.de>
2024-10-11 10:12:50 +0200

    Improve collection list style

--- frontend/src/components/collections/CollectionList.vue
+++ frontend/src/components/collections/CollectionList.vue
@@ -33,16 +33,16 @@ export default {
 </script>
 
 <template>
-  <div class="h-full w-[250px] transition-[margin] duration-300"
+  <div class="h-full w-[250px] transition-[margin] duration-300 shadow-md bg-white"
     :class="{
       '-ml-[250px]': collapsed,
       'ml-0': !collapsed,
     }">
 
     <div
-      class="h-full overflow-y-auto flex flex-col gap-3 pt-6 pb-3 shadow-md bg-white">
+      class="h-full overflow-y-auto flex flex-col gap-3 pt-6 pb-3 pl-3 pr-3">
 
-      <div class="ml-5 flex flex-row items-center text-left">
+      <div class="flex flex-row items-center text-left px-2">
         <h3 class="font-bold text-[15px]">
           Recent Collections
         </h3>
@@ -50,12 +50,9 @@ export default {
 
       <div v-for="collection in collectionStore.available_collections" :key="collection.id"
         class="flex flex-row">
-        <button
+        <button class="w-full text-left text-[15px] text-md px-2 py-1 flex flex-row items-center rounded-md hover:text-blue-500 hover:bg-gray-100"
           @click="collectionStore.open_collection(collection.id, collection.actual_classes[0].name)">
-          <h3 class="text-left text-[15px] text-md ml-3 mr-3 px-2 py-1 flex flex-row items-center rounded-md hover:text-blue-500 hover:bg-gray-100"
-            :class="{
-              'text-blue-500': collectionStore.collection_id == collection.id && collectionStore.class_name == collection.actual_classes[0].name,
-              }">
+          <h3>
             {{ collection.name }}
           </h3>
         </button>
