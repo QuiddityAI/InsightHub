285202946ba14cc50282411625bebf8c70d11dd7
Tim Henning <tim@luminosuslight.de>
2024-11-08 15:15:09 +0100

    Improve criteria prompts

--- backend/workflows/prompts.py
+++ backend/workflows/prompts.py
@@ -10,6 +10,8 @@ The query is: {{ query }}
 
 criteria_prompt_en = """
 For a given query, write a list of criteria that potential search results have to fulfill.
+Only split the query into multiple parts if it is necessary to easily evaluate the criteria.
+Do not add any additional criteria that are not included in the query.
 
 Examples:
 Query: "What is the capital of France?"
@@ -18,14 +20,23 @@ Criteria:
 
 Query: "a technical drawing of a car in a PDF file"
 Criteria:
-- contains a technical drawing
-- is about a car
+- contains a technical drawing of a car
 - is a PDF file
 
-Query: "a list of the most popular programming languages"
+Query: "a list of the most popular programming languages of 2022"
 Criteria:
-- contains a list
-- names the most popular programming languages
+- contains a list of the most popular programming languages
+- refers to the year 2022
+
+Query: "cost breakdown of facility XYZ for October 2014"
+Criteria:
+- is a project plan
+- refers to facility XYZ
+- refers to the month October 2014
+
+Query: "recycling process for hard plastic"
+Criteria:
+- is about recycling processes for hard plastic
 
 The query is: "{{ query }}"
 
@@ -35,6 +46,8 @@ Return only the criteria as a list of bullet points. Do not repeat the task.
 
 criteria_prompt_de = """
 Für eine gegebene Suchanfrage, schreibe eine Liste von Kriterien, die potenzielle Suchergebnisse erfüllen müssen.
+Teile die Anfrage nur in mehrere Teile auf, wenn es notwendig ist, um die Kriterien leicht zu bewerten.
+Füge keine weiteren Kriterien hinzu, die nicht in der Anfrage enthalten sind.
 
 Beispiele:
 Anfrage: "Was ist die Hauptstadt von Frankreich?"
@@ -43,14 +56,23 @@ Kriterien:
 
 Anfrage: "eine technische Zeichnung eines Autos in einer PDF-Datei"
 Kriterien:
-- enthält eine technische Zeichnung
-- handelt von einem Auto
+- enthält eine technische Zeichnung eines Autos
 - ist eine PDF-Datei
 
-Anfrage: "eine Liste der beliebtesten Programmiersprachen"
+Anfrage: "eine Liste der beliebtesten Programmiersprachen von 2022"
 Kriterien:
-- enthält eine Liste
-- nennt die beliebtesten Programmiersprachen
+- enthält eine Liste der beliebtesten Programmiersprachen
+- bezieht sich auf das Jahr 2022
+
+Anfrage: "Kostenabrechnung der Anlage XYZ für Oktober 2014"
+Kriterien:
+- ist ein Projektplan
+- bezieht sich auf die Anlage XYZ
+- bezieht sich auf den Monat Oktober 2014
+
+Anfrage: "Recyclingverfahren für Hartplastik"
+Kriterien:
+- handelt von Recyclingverfahren für Hartplastik
 
 Die Suchanfrage ist: "{{ query }}"
 
