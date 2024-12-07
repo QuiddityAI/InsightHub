bc411fbae00c957a3ac7194a73c034c9862660a0
Tim Henning <tim@luminosuslight.de>
2024-12-07 18:19:53 +0100

    Show Spinner during search or other processes

--- frontend/src/components/collections/AgentModeBar.vue
+++ frontend/src/components/collections/AgentModeBar.vue
@@ -3,6 +3,8 @@ import {
   NoSymbolIcon,
 } from "@heroicons/vue/24/outline"
 
+import ProgressSpinner from "primevue/progressspinner";
+
 import BorderlessButton from "../widgets/BorderlessButton.vue";
 
 import { mapStores } from "pinia"
@@ -44,6 +46,7 @@ export default {
 <template>
   <div class="w-full px-5 py-1 flex flex-row items-center gap-4">
     <span class="text-orange-500">Processing:</span>
+    <ProgressSpinner class="w-5 h-5 -mr-1" />
     <span class="text-gray-700">
       {{ collectionStore.collection.current_agent_step }}
     </span>
