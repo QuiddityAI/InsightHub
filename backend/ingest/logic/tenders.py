2fba3fd969dd5518487edb4e488dee82f671202e
Tim Henning <tim@luminosuslight.de>
2025-01-07 19:26:26 +0100

    Work on tender scraping

--- backend/ingest/logic/tenders.py
+++ backend/ingest/logic/tenders.py
@@ -9,7 +9,6 @@ from llmonkey.llms import BaseLLMModel, Nebius_Llama_3_1_8B_cheap
 from data_map_backend.utils import DotDict
 from columns.logic.website_scraping_column import scrape_website_plain
 
-
 LlmModel = Nebius_Llama_3_1_8B_cheap
 
 
@@ -21,8 +20,9 @@ class TenderInput(Schema):
 
 class TenderEnrichmentOutput(Schema):
     summary: str = ""
+    services: str = ""  # aka Leistungsverzeichnis
+    requirements: str = ""  # aka Anforderungen
     website_text: str = ""
-
 
 
 def tender_enrichment_generator(input_items: list[dict], log_error: Callable, parameters: DotDict) -> list[dict]:
@@ -53,47 +53,59 @@ def tender_enrichment_generator(input_items: list[dict], log_error: Callable, pa
 
 def enrich_tender(item: TenderInput) -> TenderEnrichmentOutput:
     if not item.url:
-        return _summarize_description(item)
+        return _summarize_tender_information(item)
     for domain in domains_not_working_with_plain_scraping + domains_serving_pdfs + domains_needing_js:
         if item.url.startswith(domain):
-            return _summarize_description(item)
+            return _summarize_tender_information(item)
     if item.website_text:
         website_text = item.website_text
     else:
+        if item.url.startswith("https://www.dtvp.de/Satellite/notice/"):
+            # https://www.dtvp.de/Satellite/notice/CXP4DCCH7ES ->
+            # https://www.dtvp.de/Satellite/public/company/project/CXP4DCCH7ES/de/processdata/eforms"
+            item.url = item.url.replace("/notice/", "/public/company/project/") + "/de/processdata/eforms"
         website_text, html = scrape_website_plain(item.url)
-        if "work-in-progress-text" in html:
-            # pages from cosinex GmbH don't work without JS and only serve PDF files
-            return _summarize_description(item)
     if not website_text:
-        return _summarize_description(item)
-    model = LlmModel()
-    system_prompt = f"""Du bekommst vom Nutzer den HTML-Text der Webseite einer Ausschreibung.
-    Fasse den Gegenstand der Ausschreibung und die zu erbringenden Leistungen in etwa 3-5 Sätzen zusammen.
-    Wenn die Informationen nicht ausreichen, antworte nur mit "n/a"."""
-    user_prompt = website_text[:10000]
-    summary = model.generate_short_text(system_prompt=system_prompt, user_prompt=user_prompt) or "n/a"
-    if summary == "n/a":
-        return _summarize_description(item)
-    return TenderEnrichmentOutput(summary=summary, website_text=website_text)
+        return _summarize_tender_information(item)
+    return _summarize_tender_information(item, website_text)
 
 
-def _summarize_description(item: TenderInput) -> TenderEnrichmentOutput:
-    if not item.description:
+def _summarize_tender_information(item: TenderInput, website_text: str | None=None) -> TenderEnrichmentOutput:
+    if not item.description and not website_text:
         return TenderEnrichmentOutput(summary="", website_text="")
     model = LlmModel()
-    system_prompt = f"""Du bekommst vom Nutzer eine Beschreibung der Ausschreibung.
-    Fasse den Gegenstand der Ausschreibung und die zu erbringenden Leistungen in etwa 3-5 Sätzen zusammen.
-    Wenn die Informationen nicht ausreichen, antworte mit "n/a"."""
-    user_prompt = item.description[:10000]
+    user_prompt = f"Beschreibung:\n{item.description[:10000] if item.description else 'n/a' or 'n/a'}\n\nWebsite Text:\n{website_text[:10000] if website_text else 'n/a' or 'n/a'}"
+
+    system_prompt = f"""Du bekommst vom Nutzer die Beschreibung und den Text einer Webseite einer Ausschreibung.
+    Fasse den Gegenstand der Ausschreibung und die zu erbringenden Leistungen in etwa 2-3 Sätzen zusammen.
+    Wenn die Informationen nicht ausreichen oder der Text nur Fehlermeldungen beinhaltet, antworte mit "n/a"."""
     summary = model.generate_short_text(system_prompt=system_prompt, user_prompt=user_prompt) or "n/a"
     if summary == "n/a":
         summary = ""
-    return TenderEnrichmentOutput(summary=summary, website_text=item.description)
 
+    system_prompt = f"""Du bekommst vom Nutzer die Beschreibung und den Text einer Webseite einer Ausschreibung.
+    Fasse die zu erbringenden Leistungen in kurzen Stichpunkten zusammen. Lasse andere Informationen weg.
+    Wenn die Informationen nicht ausreichen oder der Text nur Fehlermeldungen beinhaltet, antworte mit "n/a"."""
+    services = model.generate_short_text(system_prompt=system_prompt, user_prompt=user_prompt) or "n/a"
+    if services == "n/a":
+        services = ""
+
+    system_prompt = f"""Du bekommst vom Nutzer die Beschreibung und den Text einer Webseite einer Ausschreibung.
+    Fasse die Anforderungen an den Bieter in kurzen Stichpunkten zusammen. Lasse andere Informationen weg.
+    Wenn die Informationen nicht ausreichen oder der Text nur Fehlermeldungen beinhaltet, antworte mit "n/a"."""
+    requirements = model.generate_short_text(system_prompt=system_prompt, user_prompt=user_prompt) or "n/a"
+    if requirements == "n/a":
+        requirements = ""
+
+    return TenderEnrichmentOutput(summary=summary, website_text=website_text or "", services=services, requirements=requirements)
+
+
+# evergabe.de: paywall, aber infos sind in <pre> auf service.bund.de
 
 domains_working_with_plain_scraping = [
     "https://www.evergabe-online.de",
     "https://www.sachsen-vergabe.de",
+    "https://plattform.aumass.de",
 ]
 
 domains_not_working_with_plain_scraping = [
